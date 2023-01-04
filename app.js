3506f0009b8632f3f1cac91a343e8ad8fa6d43e7
sparshrawat34@gmail.com <sparshrawat34@gamil.com>
2023-01-04 20:53:56 +0530

    final changes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-function addRecommendation(){
-    let recommendation=document.getElementById('new_recommendation');
-    if(recommendation.value!=null && recommendation.value.trim()!=""){
-        console.log("New recommendation added");
-        showPopup(true);
-        //new recoomendation
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
+    //Call showPopup here
+    showPopup(true);
+    // Create a new 'recommendation' element and set it's value to the user's message
     var element = document.createElement("div");
     element.setAttribute("class","recommendation");
     element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
@@ -22,4 +25,3 @@ function showPopup(bool) {
     document.getElementById('popup').style.visibility = 'hidden'
   }
 }
-   
